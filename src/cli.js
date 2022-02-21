8a7fb2496581f4a033c7ebc6ff85414769089dce
Rodion <rodionrusu.md@gmail.com>
2022-02-21 20:59:56 +0200

    update cli

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 function greetings() {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May, I have your name?: ');
+  const name = readlineSync.question('May, I have your name please?: ');
   console.log(`Hello, ${name}`);
 }
 
